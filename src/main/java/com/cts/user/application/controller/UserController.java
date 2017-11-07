b25717da12e78614a574f54d8685ac92f05a05d6
Sudipta Paul <sudiptapaul1978@gmail.com>
2017-11-07 08:06:36 -0800

    Modified

--- src/main/java/com/cts/user/application/controller/UserController.java
+++ src/main/java/com/cts/user/application/controller/UserController.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,7 +22,7 @@ public class UserController {
 	private UserService userService;
 
 	@CrossOrigin
-	@PutMapping("/save")
+	@PostMapping("/register")
 	public Map<String, Object> createUser(@RequestBody UserRequest user) {
 
 		System.out.println("Saving user: " + user);
